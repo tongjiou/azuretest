5cee4a8c6f1d313c3f4548e21a991b2af3fcc08b
jiou.tong <tongjiou@users.noreply.github.com>
2025-03-01 13:33:47 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from mysql.connector import Error
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def check_mysql_connection():
+    try:
+        connection = mysql.connector.connect(
+            host='eptest-server.mysql.database.azure.com',
+            user='imhryoibrc',
+            password='octZaEIfT$QwHu$$',
+            database='eptest-database'
+        )
+        if connection.is_connected():
+            return "Connection to MySQL database was successful!"
+    except Error as e:
+        return f"Error while connecting to MySQL: {e}"
+    finally:
+        if connection.is_connected():
+            connection.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
